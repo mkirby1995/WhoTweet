e90d3dd681f8c0570c1941adb9fd50b9195aab39
Matt.Kirby <mkirby3@angelo.edu>
2019-06-17 17:43:07 -0500

    create sqlite database and add 2 uesers and 6 tweets

--- WhoTweet/app.py
+++ WhoTweet/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from .models import DB
 
 def create_app():
   """Create and configure an instance of the flask application"""
   app = Flask(__name__)
-
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.sqlite3'
+  DB.init_app(app)
+  
   @app.route('/')
   def root():
     return('Hello WhoTweet!')
